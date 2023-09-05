bb43def2768923509b5b7cf2c8ba26881c94cab2
Tibart <tibart@biglebowski.nl>
2023-09-05 21:34:23 +0200

    added obsidian-daily-notes-interface

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
 import { Notice, Plugin } from 'obsidian';
+import { getAllDailyNotes, getDailyNote } from 'obsidian-daily-notes-interface';
+import * as moment from 'moment'
 
 export default class MovingCheckbox extends Plugin {
 
@@ -10,15 +12,19 @@ export default class MovingCheckbox extends Plugin {
 				const lineNr = editor.getCursor().line
 				const selection = editor.getLine(lineNr)
 				const checkboxRe = /[-*]\s\[ ] (?!~|\[\^\d+])/g
-				if (selection.match(checkboxRe)) {
+				if (editor.somethingSelected() && selection.match(checkboxRe)) {
 					// Find next day or create next day in daily calendar
+					const tomorrow = moment().add(1, 'day')
+					const note = getDailyNote(tomorrow, getAllDailyNotes())
+					
 
 					// Copy task to next day
 
 					// Check settings what to do
+					// Check settings skip weekend
 					// Change Task to moved [>]
 
-					new Notice(selection)
+					new Notice(note.name)
 				}
 			},
 		})
