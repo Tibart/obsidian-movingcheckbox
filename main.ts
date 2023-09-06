ad07108e2e7a776d50dc02294437b2eb5d612bf2
Tibart <tibart@biglebowski.nl>
2023-09-06 22:13:12 +0200

    Load or create next daily note open on new tab

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
-import { Notice, Plugin } from 'obsidian';
-import { getAllDailyNotes, getDailyNote } from 'obsidian-daily-notes-interface';
-import * as moment from 'moment'
+import { Plugin, TFile, Workspace } from 'obsidian';
+import { getAllDailyNotes, getDailyNote, createDailyNote } from 'obsidian-daily-notes-interface';
+import moment from 'moment';
 
 export default class MovingCheckbox extends Plugin {
 
@@ -12,11 +12,20 @@ export default class MovingCheckbox extends Plugin {
 				const lineNr = editor.getCursor().line
 				const selection = editor.getLine(lineNr)
 				const checkboxRe = /[-*]\s\[ ] (?!~|\[\^\d+])/g
-				if (editor.somethingSelected() && selection.match(checkboxRe)) {
+				if (selection.match(checkboxRe)) {
+
 					// Find next day or create next day in daily calendar
-					const tomorrow = moment().add(1, 'day')
-					const note = getDailyNote(tomorrow, getAllDailyNotes())
-					
+					const tomorrow = moment().add(1, "d")
+					const nextNote = getDailyNote(tomorrow, getAllDailyNotes())
+					if (!nextNote) {
+						createDailyNote(tomorrow)
+							.then(newNote =>  {
+								openNoteNewLeaf(ctx.app.workspace, newNote)
+							}); 
+					} else {
+						openNoteNewLeaf(ctx.app.workspace, nextNote)
+		
+					}	
 
 					// Copy task to next day
 
@@ -24,14 +33,25 @@ export default class MovingCheckbox extends Plugin {
 					// Check settings skip weekend
 					// Change Task to moved [>]
 
-					new Notice(note.name)
+					//new Notice(note.name)
 				}
 			},
 		})
+
+		function openNoteNewLeaf(workspace: Workspace, note: TFile): void {
+			let exists = false;
+			workspace.iterateAllLeaves(l => { 
+				exists = exists ? exists : l.getDisplayText() === note.basename 
+			});
+	
+			if (!exists) {
+				workspace.getLeaf("tab").openFile(note, { active: false })
+			}
+		}
 	}
 
 	onunload() {
-		console.log('Unloading plugin Moving checkbox');
+		//console.log('Unloading plugin Moving checkbox');
 		
 	}
 }
