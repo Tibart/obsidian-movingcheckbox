b8c0d54a19358dea386255b23d914e830b551a81
Tibart <tibart@biglebowski.nl>
2023-10-20 21:25:45 +0200

    Implemented settings

--- MovingCheckboxSettingTab.ts
+++ MovingCheckboxSettingTab.ts
@@ -2,7 +2,8 @@ import MovingCheckbox from "main"
 import { App, Setting, PluginSettingTab } from "obsidian"
 
 export interface MovingCheckboxSettings {
-	skipWeekend: boolean
+	skipWeekend: boolean,
+    movedSign: string,
 }
 
 export class MovingCheckboxSettingTab extends PluginSettingTab {
@@ -19,6 +20,7 @@ export class MovingCheckboxSettingTab extends PluginSettingTab {
         this.containerEl.createEl('h3', { text: 'Moving checkbox settings' })
 
         this.addSkipWeekend()
+        this.addMovedSign()
     }
 
     addSkipWeekend(): void {
@@ -29,7 +31,18 @@ export class MovingCheckboxSettingTab extends PluginSettingTab {
                 .onChange( async v => {
                     this.plugin.settings.skipWeekend = v
                     await this.plugin.saveSettings()
-                })
-            )
+                }))
+    }
+
+    addMovedSign(): void {
+        new Setting(this.containerEl)
+            .setName('Moved marked sign')
+            .setDesc("What marked sign should the checkbox have when it is moved. Traditionally is is marked with a 'x' what causes the checkbox to strikeout. Default for this plugin is '>'. This does not strikeout the checkbox and indicated a movement to the next day according to some bullet journal conventions.")
+            .addText(t => t
+                .setValue(this.plugin.settings.movedSign)
+                .onChange( async v => {
+                    this.plugin.settings.movedSign = v
+                    await this.plugin.saveSettings()
+                }))
     }
 }
--- main.ts
+++ main.ts
@@ -1,63 +1,84 @@
-import { Plugin, TFile, Workspace } from 'obsidian'; 
+import { Editor, Plugin, TFile, Workspace } from 'obsidian';
 import { getAllDailyNotes, getDailyNote, createDailyNote, getDailyNoteSettings, getTemplateInfo } from 'obsidian-daily-notes-interface';
-import { MovingCheckboxSettingTab, MovingCheckboxSettings 	} from 'MovingCheckboxSettingTab';
+import { MovingCheckboxSettingTab, MovingCheckboxSettings } from 'MovingCheckboxSettingTab';
 import moment from 'moment';
+import { Context } from 'vm';
 
-const DEFAULT_SETTINGS: MovingCheckboxSettings = {
-	skipWeekend: true,
-}
+
 
 export default class MovingCheckbox extends Plugin {
 	settings: MovingCheckboxSettings
 
+	async loadSettings() {
+		const DEFAULT_SETTINGS: MovingCheckboxSettings = {
+			skipWeekend: true,
+			movedSign: '>'
+		}
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+	
 	async onload() {
 		await this.loadSettings()
-		this.addSettingTab( new MovingCheckboxSettingTab(this.app, this)) 
-
+		this.addSettingTab(new MovingCheckboxSettingTab(this.app, this))
+		const settings = this.settings
+		
 		this.addCommand({
 			id: "select_task",
 			name: "Select current task",
 			async editorCallback(editor, ctx) {
-				const cursor = editor.getCursor()
-				const selection = editor.getLine(cursor.line)
-				const checkboxRe = /[-*]\s\[ ] (?!~|\[\^\d+])/g
-				if (selection.match(checkboxRe)) {
-
-					// Find next day or create next day in daily calendar
-					const tomorrow = moment().add(1, "d")
-					let nextNote: TFile = getDailyNote(tomorrow, getAllDailyNotes())
-					if (!nextNote) {
-						nextNote = await createDailyNote(tomorrow)
-					}	
-					
-					// Copy task to next day
-					let nextNoteContent = await ctx.app.vault.read(nextNote);
-					// TODO: insert checkbox at the right position
-					
-					nextNoteContent = nextNoteContent + selection + '\n'
-					ctx.app.vault.modify(nextNote, nextNoteContent)
-					
-					// Set existing task to completed
-					editor.setLine(cursor.line, selection.replace('[ ]', '[>]'))
-				
-					
-					
-					// Check settings what to do
-					// Check settings skip weekend
-					// Change Task to moved [>]
-					
-					//openNoteNewLeaf(ctx.app.workspace, nextNote)
-				}
-			},
+				await MoveCheckBox(editor, ctx)
+			}
 		})
+		
+		async function MoveCheckBox(editor: Editor, ctx: Context) {
+			const cursor = editor.getCursor()
+			const selection = editor.getLine(cursor.line)
+			const checkboxRe = /[-*]\s\[ ] (?!~|\[\^\d+])/g
+
+			if (selection.match(checkboxRe)) {
+				const nextDay = moment().add(skipDays(), "d")
+				let nextNote: TFile = getDailyNote(nextDay, getAllDailyNotes())
+				if (!nextNote) {
+					nextNote = await createDailyNote(nextDay)
+				}
+
+				// Copy task to next day
+				let nextNoteContent = await ctx.app.vault.read(nextNote);
+				
+				// TODO: insert checkbox at the right position
+				nextNoteContent = nextNoteContent + selection + '\n'
+				ctx.app.vault.modify(nextNote, nextNoteContent)
+				
+				// Set existing task to completed
+				editor.setLine(cursor.line, selection.replace('[ ]', '[' + settings.movedSign + ']'))
+
+				//openNoteNewLeaf(ctx.app.workspace, nextNote)
+			}
+		}
+
+		function skipDays() { 
+			let days = 1
+
+			if (settings.skipWeekend)
+			{
+				const dotw = moment().weekday(); 
+				days = dotw >= 5 ? 8 - dotw : 1
+			}
+
+			return days
+		}
 
 		// TODO: move to separate file
 		function openNoteNewLeaf(workspace: Workspace, note: TFile): void {
 			let exists = false;
-			workspace.iterateAllLeaves(l => { 
-				exists = exists ? exists : l.getDisplayText() === note.basename 
+			workspace.iterateAllLeaves(l => {
+				exists = exists ? exists : l.getDisplayText() === note.basename
 			});
-	
+
 			if (!exists) {
 				workspace.getLeaf("tab").openFile(note, { active: false })
 			}
@@ -66,19 +87,9 @@ export default class MovingCheckbox extends Plugin {
 
 	onunload() {
 		//console.log('Unloading plugin Moving checkbox');
-		
+
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
 
-	async test(): Promise<void> {
-		console.log("Test");
-		
-	}
 }
